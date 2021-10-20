bd3355de89e72bfb23d8d6940c07c9ae25e8a4bc
Neil Ning <ningcoder@foxmail.com>
2021-10-20 23:54:04 +0800

    Update package.json

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
+  testURL: 'http://localhost/',
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/__mocks__/fileMock.js',
-    '\\.(css|less|scss)$': 'identity-obj-proxy',
+      '<rootDir>/.erb/mocks/fileMock.js',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
   },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+  moduleDirectories: ['node_modules', 'build/app/node_modules'],
+  setupFiles: ['./.erb/scripts/check-build-exists.js'],
   setupFilesAfterEnv: ['./jest.setup.js'],
 };
